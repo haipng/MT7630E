b12e42cb4f460ed1f1822e578d840557cf591f3b
haipng <haiphamece@gmail.com>
2022-08-19 14:25:38 +0000

    Fix build error (missing header file)

--- rt2x00/rt2x00.h
+++ rt2x00/rt2x00.h
@@ -36,7 +36,7 @@
 #include <linux/leds.h>
 #include <linux/mutex.h>
 #include <linux/etherdevice.h>
-#include <linux/input-polldev.h>
+//#include <linux/input-polldev.h>
 #include <linux/kfifo.h>
 #include <linux/hrtimer.h>
 #include <linux/version.h>
